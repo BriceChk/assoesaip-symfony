6a266dc1b0969b48145d586c5f9a77460bd67fec
Brice Chkir <brice.chkir@outlook.fr>
2020-10-22 18:43:48 +0200

    Category news and calendar

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\News;
 use App\Entity\ProjectCategory;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
@@ -20,8 +21,20 @@ class CategoryController extends AbstractController
             throw $this->createNotFoundException("Cette catégorie n'existe pas");
         }
 
+        $user = $this->getUser();
+
+        //TODO Ajouter la gestion des pages (limiter le nombre de news)
+        $news = array();
+        foreach ($categ->getProjects() as $p) {
+            if ($user != null && $p->getCampus() != $user->getCampus() && !$this->isGranted('ROLE_ADMIN')) {
+                continue;
+            }
+            $news = array_merge($news, $p->getNews()->toArray());
+        }
+
         return $this->render('category.html.twig', [
             'category' => $categ,
+            'news' => $news
         ]);
     }
 }
