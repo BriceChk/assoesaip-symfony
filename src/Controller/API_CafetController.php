72306d4a54750ce337d5ab31192f66795c5b1191
Brice Chkir <brice.chkir@outlook.fr>
2021-01-13 22:10:16 +0100

    Basic Cafet API

---
+++ src/Controller/API_CafetController.php
@@ -0,0 +1,35 @@
+<?php
+namespace App\Controller;
+
+use App\Entity\CafetItem;
+use FOS\RestBundle\Controller\AbstractFOSRestController;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use FOS\RestBundle\Controller\Annotations\View;
+use Nelmio\ApiDocBundle\Annotation\Model;
+use OpenApi\Annotations as OA;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+
+class API_CafetController extends AbstractFOSRestController {
+
+    /**
+     * Get the list of CafetItems.
+     * @OA\Response (
+     *     response = 200,
+     *     description = "Returns the CafetItem list",
+     *     @OA\JsonContent(type="array", @OA\Items(ref=@Model(type=CafetItem::class)))
+     * )
+     * @OA\Tag(name="Cafet")
+     * @Rest\Get(
+     *     path = "/api/cafet/",
+     *     name = "api_cafet_list",
+     *     requirements = { "id"="\d+" }
+     * )
+     * @View
+     * @IsGranted("ROLE_USER")
+     * @return CafetItem[]|object[]
+     */
+    public function getCafetItemsList(): array {
+        $rep = $this->getDoctrine()->getRepository(CafetItem::class);
+        return $rep->findAll();
+    }
+}
---
+++ src/Entity/CafetItem.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CafetItemRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=CafetItemRepository::class)
+ */
+class CafetItem
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $imageFileName;
+
+    /**
+     * @ORM\Column(type="string", length=10)
+     */
+    private $type;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $price;
+
+    /**
+     * @ORM\Column(type="string", length=42)
+     */
+    private $day;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getImageFileName(): ?string
+    {
+        return $this->imageFileName;
+    }
+
+    public function setImageFileName(string $imageFileName): self
+    {
+        $this->imageFileName = $imageFileName;
+
+        return $this;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): self
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getPrice(): ?float
+    {
+        return $this->price;
+    }
+
+    public function setPrice(float $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getDay(): ?string
+    {
+        return $this->day;
+    }
+
+    public function setDay(string $day): self
+    {
+        $this->day = $day;
+
+        return $this;
+    }
+}
