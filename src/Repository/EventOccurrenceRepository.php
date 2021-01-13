15925818c873fab0fdb61023bc9e29f529eff20e
Brice Chkir <brice.chkir@outlook.fr>
2021-01-13 22:11:28 +0100

    Next occurrences

--- src/Repository/EventOccurrenceRepository.php
+++ src/Repository/EventOccurrenceRepository.php
@@ -4,6 +4,9 @@ namespace App\Repository;
 
 use App\Entity\Event;
 use App\Entity\EventOccurrence;
+use App\Entity\Project;
+use App\Entity\ProjectCategory;
+use DateTime;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
@@ -45,6 +48,7 @@ class EventOccurrenceRepository extends ServiceEntityRepository
      */
     public function findBetweenDates($start, $end)
     {
+        //TODO Campus ???
         return $this->createQueryBuilder('e')
             ->andWhere('e.date >= :start')
             ->setParameter('start', $start)
@@ -52,6 +56,77 @@ class EventOccurrenceRepository extends ServiceEntityRepository
             ->setParameter('end', $end)
             ->orderBy('e.id', 'ASC')
             ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+            ;
+    }
+
+    /**
+     * @param string $campus
+     * @return EventOccurrence[] Returns an array of EventOccurrence objects
+     */
+    public function findNext($campus = "A")
+    {
+        return $this->createQueryBuilder('e')
+            ->innerJoin('e.event', 'ev')
+            ->innerJoin('ev.project', 'p')
+            ->andWhere('e.date >= :start')
+            ->andWhere('ev.published = :pub')
+            ->andWhere('ev.private = :priv')
+            ->andWhere('p.campus LIKE :camp')
+            ->setParameter('start', new DateTime('now'))
+            ->setParameter('pub', true)
+            ->setParameter('priv', false)
+            ->setParameter('camp', '%' . $campus . '%')
+            ->orderBy('e.date', 'ASC')
+            ->getQuery()
+            ->getResult()
+            ;
+    }
+
+    /**
+     * @param ProjectCategory $categ
+     * @param string $campus
+     * @return EventOccurrence[] Returns an array of EventOccurrence objects
+     */
+    public function findCategoryNext(ProjectCategory $categ, $campus = "A")
+    {
+        return $this->createQueryBuilder('e')
+            ->innerJoin('e.event', 'ev')
+            ->innerJoin('ev.project', 'p')
+            ->andWhere('e.date >= :start')
+            ->andWhere('ev.published = :pub')
+            ->andWhere('ev.private = :priv')
+            ->andWhere('p.campus LIKE :camp')
+            ->andWhere('p.category = :categ')
+            ->setParameter('start', new DateTime('now'))
+            ->setParameter('pub', true)
+            ->setParameter('priv', false)
+            ->setParameter('camp', '%' . $campus . '%')
+            ->setParameter('categ', $categ)
+            ->orderBy('e.date', 'ASC')
+            ->getQuery()
+            ->getResult()
+            ;
+    }
+
+    /**
+     * @param Project $project
+     * @return EventOccurrence[] Returns an array of EventOccurrence objects
+     */
+    public function findProjectNext(Project $project)
+    {
+        return $this->createQueryBuilder('e')
+            ->innerJoin('e.event', 'ev')
+            ->andWhere('ev.project = :proj')
+            ->andWhere('e.date >= :start')
+            ->andWhere('ev.published = :pub')
+            ->andWhere('ev.private = :priv')
+            ->setParameter('proj', $project)
+            ->setParameter('start', new DateTime('now'))
+            ->setParameter('pub', true)
+            ->setParameter('priv', false)
+            ->orderBy('e.date', 'ASC')
             ->getQuery()
             ->getResult()
             ;
